d0b07a87287fcace3e5df549c3274cef92ae7049
“German <gangaritah@gmail.com>
2023-08-02 07:27:57 -0500

    ADD token, cookie to client

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const signingKey = require('../config/keys');
+const signingKey = require('../config/keys');
 
 
 let auth = (req, res) => {
@@ -7,9 +8,22 @@ let auth = (req, res) => {
       let password = req.body.password
       console.log("Email", email);
       console.log("Password", password);
-      return res.status(200).send(
-        { status: 'authentication ok', auth: true}
-      );
+
+      let token = generateToken(
+        { email: email }, signingKey.SIGNING_KEY,
+        new Date().getTime() + (2 * 60 * 1000)
+        );
+
+
+      let cookieConfig = {
+                            domain: 'localhost', path: '/refresh', secure: false,
+                            expires: new Date(Date.now() + 300000), httpOnly: true
+                          }
+
+      return res.status(200).cookie('refresh_token', email, cookieConfig)
+                              .json({ 
+                                status: 'Successful authentication', token: token
+                              });
      
 }
 
